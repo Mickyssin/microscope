e3613f568f800df1604c42552d280ed0a5dc4a53
Mickyssin <micky_moco_@hotmail.com>
2017-06-09 09:07:58 -0600

    Tests añadadidos al paquete.

--- packages/errors/errors-tests.js
+++ packages/errors/errors-tests.js
@@ -1,11 +1,21 @@
-// Import Tinytest from the tinytest Meteor package.
-import { Tinytest } from "meteor/tinytest";
+Tinytest.add("Errors - collection", function(test) {
+  test.equal(Errors.collection.find({}).count(), 0);
+  
+  Errors.throw('A new error!');
+  test.equal(Errors.collection.find({}).count(), 1);
+  
+  Errors.collection.remove({});
+});
 
-// Import and rename a variable exported by errors.js.
-import { name as packageName } from "meteor/tmeasday:errors";
-
-// Write your tests here!
-// Here is an example.
-Tinytest.add('errors - example', function (test) {
-  test.equal(packageName, "errors");
+Tinytest.addAsync("Errors - template", function(test, done) {  
+  Errors.throw('A new error!');
+  test.equal(Errors.collection.find({}).count(), 1);
+  
+  // render the template
+  UI.insert(UI.render(Template.meteorErrors), document.body);
+  
+  Meteor.setTimeout(function() {
+    test.equal(Errors.collection.find({}).count(), 0);
+    done();
+  }, 3500);
 });
--- packages/errors/package.js
+++ packages/errors/package.js
@@ -6,11 +6,18 @@ Package.describe({
 
 Package.onUse(function (api, where) {
   api.versionsFrom('0.9.0');
-
+  
   api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
 
   api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
-
-  if (api.export)
+  
+  if (api.export) 
     api.export('Errors');
+});
+
+Package.onTest(function(api) {
+  api.use('tmeasday:errors', 'client');
+  api.use(['tinytest', 'test-helpers'], 'client');  
+  
+  api.addFiles('errors_tests.js', 'client');
 });
